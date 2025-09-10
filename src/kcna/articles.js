76d537f5d852c7bbad6d3528f043e14104bc86f5
George H. Thomas <georgebushdontcare@gmail.com>
2025-09-10 16:22:15 -0400

    dev

--- src/kcna/articles.js
+++ src/kcna/articles.js
@@ -1,3 +1,13 @@
+import CONFIG from "../../config/config.js";
+
+//get article urls
 export const scrapeArticlesKCNA = async () => {
-    console.log("ALLAHU AKBAR")
+  const { articleTypeUrlArr } = CONFIG;
+
+  for (const type of articleTypeUrlArr) {
+    const { url } = CONFIG[type];
+    console.log("ARTICLE URL");
+    console.log(url);
+    // const articleUrls = await scrapeArticleUrls(url);
+  }
 };
