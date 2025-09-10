f002d4855563b5ef293d45c845ab4c29ff753068
George H. Thomas <georgebushdontcare@gmail.com>
2025-09-10 15:36:15 -0400

    dev

--- src/control.js
+++ src/control.js
@@ -1,4 +1,17 @@
 export const handleAdminCommand = async (inputParams) => {
+  const { commandType } = inputParams;
+
+  //prob not necessary to track data
+  let data = "";
+  switch (commandType) {
+    case "admin-start-scrape":
+      //ADD check to see if scrape already running
+      data = await runNewScrape(inputParams);
+      return data;
+
+    default:
+      return null;
+  }
   console.log("API DATA");
   console.log(inputParams);
 };
