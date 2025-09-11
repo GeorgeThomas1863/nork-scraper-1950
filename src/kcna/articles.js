95b34e704432ae76635dd5e279c45eff7836764b
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-09-10 20:24:21 -0400

    dev

--- src/kcna/articles.js
+++ src/kcna/articles.js
@@ -2,31 +2,44 @@ import { JSDOM } from "jsdom";
 
 import CONFIG from "../../config/config.js";
 import KCNA from "../../models/kcna.js";
+import dbModel from "../../models/db-model.js";
+import { kcnaState } from "./kcna-state.js";
+
+export const scrapeArticlesKCNA = async () => {
+  const articleURLArray = await scrapeArticleURLs();
+  console.log("ARTICLE URLS");
+  console.log(articleURLArray);
+};
 
 //get article urls
-export const scrapeArticlesKCNA = async () => {
-  const { articleTypeUrlArr } = CONFIG;
+export const scrapeArticleURLs = async () => {
+  const { articleTypeUrlArr, articles } = CONFIG;
 
-  console.log("ARTICLE TYPE URL ARRAY");
-  console.log(articleTypeUrlArr);
-
+  const articleURLArray = [];
   for (const type of articleTypeUrlArr) {
     try {
       const typeURL = CONFIG[type];
-      // console.log("TYPE URL");
-      // console.log(typeURL);
+
       const kcna = new KCNA({ url: typeURL });
-      //list html
       const html = await kcna.getHTML();
-      const articleArray = await extractArticleList(html, type);
+      const articleListArray = await extractArticleList(html, type);
 
-      //   console.log("ARTICLE ARRAY");
-      //   console.log(articleArray);
-
-      for (const a of articleArray) {
+      //loop through each article and store
+      for (const a of articleListArray) {
         const articleURL = "http://www.kcna.kp" + a;
-        console.log("ARTICLE URL");
-        console.log(articleURL);
+        const params = {
+          url: articleURL,
+          articleType: type,
+          scrapeId: kcnaState.scrapeId,
+        };
+
+        const storeModel = new dbModel(params, articles);
+        const storeData = await storeModel.storeUniqueURL();
+        console.log("STORE DATA");
+        console.log(storeData);
+
+        //for tracking
+        articleURLArray.push(params);
       }
     } catch (e) {
       console.log(e.message + "; URL: " + e.url + "; ARTICLE TYPE: " + e.articleType + "; F BREAK: " + e.function);
@@ -75,97 +88,3 @@ export const extractArticleList = async (html, type) => {
 
   return articleArray;
 };
-
-// export const extractArticleLinkArray = async (inputArray) => {
-
-//     const articleLinkArray = [];
-//     for (const linkElement of linkElementArray) {
-//         const articleLink = linkElement.getAttribute("href");
-//         articleLinkArray.push(articleLink);
-//     }
-//     return articleLinkArray;
-// }
-
-// // Find article-link (wrapper element) extract out all links
-
-// const articleLinkObj = {
-//   type: type,
-//   inputArray: linkElementArray,
-// };
-
-// //extract out the articleListObjs
-// const articleLinkModel = new Article(articleLinkObj);
-// const articleListArray = await articleLinkModel.parseArticleLinks();
-
-// return articleListArray;
-
-// /**
-// * Parses array of article link items (loops through), returns array of (unsorted) articleListObjs
-// * @function parseLinkArray
-// * @param {*} inputArray (array of article link items)
-// * @returns //array of (unsorted) articleListObjs
-// */
-// async parseArticleLinks() {
-// const { inputArray, type } = this.dataObject;
-
-// //loop through a tags and pull out hrefs
-// const articleListArray = [];
-// for (let i = 0; i < inputArray.length; i++) {
-//   try {
-//     const articleListModel = new Article({ listItem: inputArray[i] });
-//     const articleListObj = await articleListModel.parseArticleListItem();
-
-//     //ADD ARTICLE TYPE HERE AND SCRAPE ID HERE
-//     articleListObj.articleType = type;
-//     articleListObj.scrapeId = scrapeState.scrapeId;
-
-//     articleListArray.push(articleListObj); //add to array
-//   } catch (e) {
-//     console.log(e.url + "; " + e.message + "; F BREAK: " + e.function);
-//   }
-// }
-
-// return articleListArray;
-// }
-
-// /**
-// * Parses individual article link item, builds / returns single articleListObj
-// * @function parseArticleListItem
-// * @param {*} listItem article link item
-// * @returns articleListObj (with url / date extracted)
-// */
-// async parseArticleListItem() {
-// const { listItem } = this.dataObject;
-// if (!listItem) return null;
-
-// //get article link
-// const href = listItem.getAttribute("href");
-
-// //throw error if cant extact pic link
-// if (!href) {
-//   const error = new Error("CANT FIND ARTICLE LINK [ARTICLE MODEL]");
-//   error.url = listItem.textContent;
-//   error.function = "parsePicSetListItem";
-//   throw error;
-// }
-
-// //build full url
-// const urlConstant = "http://www.kcna.kp";
-// const url = urlConstant + href;
-
-// //CHECK IF URL NEW HERE, throws error if not new
-// const checkModel = new dbModel({ url: url }, CONFIG.articleList);
-// await checkModel.urlNewCheck();
-
-// //GET DATE
-// const dateModel = new UTIL({ inputItem: listItem });
-// const articleDate = await dateModel.parseListDate();
-
-// //build obj
-// const articleListObj = {
-//   url: url,
-//   date: articleDate,
-// };
-
-// return articleListObj;
-// }
