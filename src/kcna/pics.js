b5292fdc4c7493510952632feae3b0ebb5874fdc
George H. Thomas <georgebushdontcare@gmail.com>
2025-09-13 16:33:33 -0400

    dev

--- src/kcna/pics.js
+++ src/kcna/pics.js
@@ -1,5 +1,10 @@
+import { JSDOM } from "jsdom";
+
 import CONFIG from "../../config/config.js";
 import NORK from "../../models/nork-model.js";
+import dbModel from "../../models/db-model.js";
+import { kcnaState } from "./kcna-state.js";
+import { extractItemDate } from "./util.js";
 
 export const scrapePicsKCNA = async () => {
   const picSetURLs = await scrapePicSetURLs();
@@ -8,14 +13,29 @@ export const scrapePicsKCNA = async () => {
 };
 
 export const scrapePicSetURLs = async () => {
-  const { picListURL } = CONFIG;
+  const { picListURL, picSets } = CONFIG;
 
   try {
-    const kcna = new NORK({ url: picListURL });
-    const html = await kcna.getHTML();
-    const picSetListArray = await parsePicSetList(html);
+    const picSetListArray = await parsePicSetList(picListURL);
 
     const picSetURLs = [];
+    for (const picSet of picSetListArray) {
+      const { picSetLink, picSetDate } = picSet;
+      const picSetURL = "http://www.kcna.kp" + picSetLink;
+      const params = {
+        url: picSetURL,
+        date: picSetDate,
+        scrapeId: kcnaState.scrapeId,
+      };
+
+      const storeModel = new dbModel(params, picSets);
+      const storeData = await storeModel.storeUniqueURL();
+      console.log("STORE DATA");
+      console.log(storeData);
+
+      picSetURLs.push(params);
+    }
+
     return picSetURLs;
   } catch (e) {
     console.log(e.message + "; URL: " + e.url + "; F BREAK: " + e.function);
@@ -23,15 +43,46 @@ export const scrapePicSetURLs = async () => {
   }
 };
 
-export const parsePicSetList = async (html) => {
-  console.log("PIC SET LIST HTML");
-  console.log(html);
+export const parsePicSetList = async (url) => {
+  if (!url) return null;
+
+  const kcna = new NORK({ url });
+  const html = await kcna.getHTML();
 
   if (!html) {
-    const error = new Error("FAILED TO GET ARTICLE LIST HTML ");
-    error.url = CONFIG[type];
-    error.articleType = type;
-    error.function = "parseArticleList";
+    const error = new Error("FAILED TO GET PIC SET LIST HTML ");
+    error.url = url;
+    error.function = "parsePicSetList";
     throw error;
   }
+
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+
+  const photoWrapperArray = document.querySelectorAll(".photo-wrapper");
+
+  //throw error if no pic Pages found
+  if (!photoWrapperArray || !photoWrapperArray.length) {
+    const error = new Error("CANT EXTRACT PICSET LIST");
+    error.url = url;
+    error.function = "parsePicSetList";
+    throw error;
+  }
+
+  const picSetArray = [];
+  for (const picSetElement of photoWrapperArray) {
+    const picSetLink = picSetElement.getAttribute("href");
+    const picSetDate = await extractItemDate(picSetElement);
+    picSetArray.push({ picSetLink, picSetDate });
+  }
+
+  //throw error if no links found
+  if (!picSetArray || !picSetArray.length) {
+    const error = new Error("CANT EXTRACT ARTICLES FROM ELEMENT");
+    error.url = url;
+    error.function = "parsePicSetList";
+    throw error;
+  }
+
+  return picSetArray;
 };
