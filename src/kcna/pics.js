0148b3cdf03fbe021bd89c6bfcffc9b9f15ff320
George H. Thomas <georgebushdontcare@gmail.com>
2025-09-13 20:58:55 -0400

    dev

--- src/kcna/pics.js
+++ src/kcna/pics.js
@@ -7,6 +7,8 @@ import { kcnaState } from "./kcna-state.js";
 import { extractItemDate } from "./util.js";
 
 export const scrapePicsKCNA = async () => {
+  const { picSets } = CONFIG;
+
   const picSetURLs = await scrapePicSetURLs();
   console.log("NEW PIC SET URLS");
   console.log(picSetURLs);
@@ -143,116 +145,116 @@ export const parsePicSetContent = async (url) => {
   console.log("PIC SETHTML");
   console.log(html);
 
-//   const dom = new JSDOM(html);
-//   const document = dom.window.document;
+  //   const dom = new JSDOM(html);
+  //   const document = dom.window.document;
 
-//   const articleTitle = await extractArticleTitle(document);
-//   const articleText = await extractArticleText(document);
-//   const articlePicPage = await extractArticlePicPage(document);
-//   const articlePicArray = await extractArticlePicArray(articlePicPage);
+  //   const articleTitle = await extractArticleTitle(document);
+  //   const articleText = await extractArticleText(document);
+  //   const articlePicPage = await extractArticlePicPage(document);
+  //   const articlePicArray = await extractArticlePicArray(articlePicPage);
 
-//   const storeParams = {
-//     title: articleTitle,
-//     text: articleText,
-//     picPageURL: articlePicPage,
-//   };
+  //   const storeParams = {
+  //     title: articleTitle,
+  //     text: articleText,
+  //     picPageURL: articlePicPage,
+  //   };
 
-//   if (articlePicArray) {
-//     storeParams.picArray = articlePicArray;
-//   }
+  //   if (articlePicArray) {
+  //     storeParams.picArray = articlePicArray;
+  //   }
 
-//   const storeModel = new dbModel({ keyToLookup: "url", itemValue: url, updateObj: storeParams }, articles);
-//   const storeData = await storeModel.updateObjItem();
-//   console.log("STORE DATA");
-//   console.log(storeData);
+  //   const storeModel = new dbModel({ keyToLookup: "url", itemValue: url, updateObj: storeParams }, articles);
+  //   const storeData = await storeModel.updateObjItem();
+  //   console.log("STORE DATA");
+  //   console.log(storeData);
 
-//   return storeParams;
-// };
+  //   return storeParams;
+  // };
 
-// export const extractArticleTitle = async (document) => {
-//   const titleElement = document.querySelector(".article-main-title");
-//   const articleTitle = titleElement?.textContent?.replace(/\s+/g, " ").trim();
-//   return articleTitle;
-// };
+  // export const extractArticleTitle = async (document) => {
+  //   const titleElement = document.querySelector(".article-main-title");
+  //   const articleTitle = titleElement?.textContent?.replace(/\s+/g, " ").trim();
+  //   return articleTitle;
+  // };
 
-// export const extractArticleText = async (document) => {
-//   //extract text content
-//   const textElement = document.querySelector(".content-wrapper");
-//   const textArray = textElement.querySelectorAll("p"); //array of paragraph elements
+  // export const extractArticleText = async (document) => {
+  //   //extract text content
+  //   const textElement = document.querySelector(".content-wrapper");
+  //   const textArray = textElement.querySelectorAll("p"); //array of paragraph elements
 
-//   const paragraphArray = [];
-//   for (let i = 0; i < textArray.length; i++) {
-//     paragraphArray.push(textArray[i].textContent.trim());
-//   }
+  //   const paragraphArray = [];
+  //   for (let i = 0; i < textArray.length; i++) {
+  //     paragraphArray.push(textArray[i].textContent.trim());
+  //   }
 
-//   // Join paragraphs with double newlines for better readability
-//   const articleText = paragraphArray.join("\n\n");
-//   return articleText;
-// };
+  //   // Join paragraphs with double newlines for better readability
+  //   const articleText = paragraphArray.join("\n\n");
+  //   return articleText;
+  // };
 
-// export const extractArticlePicPage = async (document) => {
-//   //get article PAGE (if exists) where all pics are displayed
-//   const mediaIconElement = document.querySelector(".media-icon");
-//   const picPageHref = mediaIconElement?.firstElementChild?.getAttribute("href");
+  // export const extractArticlePicPage = async (document) => {
+  //   //get article PAGE (if exists) where all pics are displayed
+  //   const mediaIconElement = document.querySelector(".media-icon");
+  //   const picPageHref = mediaIconElement?.firstElementChild?.getAttribute("href");
 
-//   //return null if  pic doesnt exist
-//   if (!picPageHref) return null;
+  //   //return null if  pic doesnt exist
+  //   if (!picPageHref) return null;
 
-//   //otherwise build pic / pic array
-//   const picPageURL = "http://www.kcna.kp" + picPageHref;
-//   return picPageURL;
-// };
+  //   //otherwise build pic / pic array
+  //   const picPageURL = "http://www.kcna.kp" + picPageHref;
+  //   return picPageURL;
+  // };
 
-// export const extractArticlePicArray = async (url) => {
-//   const { pics } = CONFIG;
-//   const { scrapeId } = kcnaState;
-//   if (!url) return null;
+  // export const extractArticlePicArray = async (url) => {
+  //   const { pics } = CONFIG;
+  //   const { scrapeId } = kcnaState;
+  //   if (!url) return null;
 
-//   try {
-//     const kcna = new NORK({ url });
-//     const html = await kcna.getHTML();
+  //   try {
+  //     const kcna = new NORK({ url });
+  //     const html = await kcna.getHTML();
 
-//     if (!html) {
-//       const error = new Error("FAILED TO GET ARTICLE PIC ARRAY HTML ");
-//       error.url = url;
-//       error.function = "extractArticlePicArray";
-//       throw error;
-//     }
+  //     if (!html) {
+  //       const error = new Error("FAILED TO GET ARTICLE PIC ARRAY HTML ");
+  //       error.url = url;
+  //       error.function = "extractArticlePicArray";
+  //       throw error;
+  //     }
 
-//     const dom = new JSDOM(html);
-//     const document = dom.window.document;
+  //     const dom = new JSDOM(html);
+  //     const document = dom.window.document;
 
-//     //get and loop through img elements
-//     const picArray = [];
-//     const imgArray = document.querySelectorAll("img");
-//     for (let i = 0; i < imgArray.length; i++) {
-//       try {
-//         const imgSrc = imgArray[i].getAttribute("src");
-//         if (!imgSrc) continue;
+  //     //get and loop through img elements
+  //     const picArray = [];
+  //     const imgArray = document.querySelectorAll("img");
+  //     for (let i = 0; i < imgArray.length; i++) {
+  //       try {
+  //         const imgSrc = imgArray[i].getAttribute("src");
+  //         if (!imgSrc) continue;
 
-//         const articlePicURL = "http://www.kcna.kp" + imgSrc;
+  //         const articlePicURL = "http://www.kcna.kp" + imgSrc;
 
-//         picArray.push(articlePicURL);
+  //         picArray.push(articlePicURL);
 
-//         const picDate = await lookupItemDate(url, "articles");
+  //         const picDate = await lookupItemDate(url, "articles");
 
-//         //store url to picDB (so dont have to do again); build params
-//         const storeParams = {
-//           url: articlePicURL,
-//           scrapeId: scrapeId,
-//           date: picDate,
-//         };
+  //         //store url to picDB (so dont have to do again); build params
+  //         const storeParams = {
+  //           url: articlePicURL,
+  //           scrapeId: scrapeId,
+  //           date: picDate,
+  //         };
 
-//         const storePicModel = new dbModel(storeParams, pics);
-//         await storePicModel.storeUniqueURL();
-//       } catch (e) {
-//         console.log(e.url + "; " + e.message + "; F BREAK: " + e.function);
-//       }
-//     }
+  //         const storePicModel = new dbModel(storeParams, pics);
+  //         await storePicModel.storeUniqueURL();
+  //       } catch (e) {
+  //         console.log(e.url + "; " + e.message + "; F BREAK: " + e.function);
+  //       }
+  //     }
 
-//     return picArray;
-//   } catch (e) {
-//     console.log(e.message + "; URL: " + e.url + "; F BREAK: " + e.function);
-//     return null;
-//   }
+  //     return picArray;
+  //   } catch (e) {
+  //     console.log(e.message + "; URL: " + e.url + "; F BREAK: " + e.function);
+  //     return null;
+  //   }
 };
