edeaa650fe1855657f3e4280a2d5d6bf054c3299
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-02 21:32:52 -0400

    dev

--- src/kcna/pics.js
+++ src/kcna/pics.js
@@ -2,12 +2,28 @@ import CONFIG from "../../config/config.js";
 import dbModel from "../../models/db-model.js";
 
 export const downloadPicsKCNA = async () => {
-  const { pics } = CONFIG;
+  const { pics, picPath } = CONFIG;
   const picModel = new dbModel({ keyExists: "url", keyEmpty: "picSize" }, pics);
   const picArray = await picModel.findEmptyItems();
 
-  console.log("PIC TO DOWNLOAD ARRAY");
-  console.log(picArray);
+  //CREATE PIC ID AND SAVE IT EARLIER IN PIC DB (maybe make last couple chars in URL?)
+  for (const picItem of picArray) {
+    const { url } = picItem;
+    const picData = await downloadPicFS(url, savePath);
+
+    console.log("PIC TO DOWNLOAD ARRAY");
+    console.log(picArray);
+  }
+};
+
+export const downloadPicFS = async (url, savePath) => {
+  const picExists = fs.existsSync(savePath);
+  if (picExists) {
+    const error = new Error("PIC ALREADY DOWNLOADED");
+    error.url = url;
+    error.function = "downloadPicFS";
+    throw error;
+  }
 };
 
 export const uploadPicsKCNA = async () => {
