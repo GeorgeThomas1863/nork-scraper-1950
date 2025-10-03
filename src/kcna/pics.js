cc6ac6e786396adb0425fabc4e52211a5e07df4b
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-02 20:08:04 -0400

    dev

--- src/kcna/pics.js
+++ src/kcna/pics.js
@@ -1,5 +1,13 @@
+import CONFIG from "../../config/config.js";
+import dbModel from "../../models/db-model.js";
+
 export const downloadPicsKCNA = async () => {
-  //build
+  const { pics } = CONFIG;
+  const picModel = new dbModel({ keyExists: "url", keyEmpty: "picSize" }, pics);
+  const picArray = await picModel.findEmptyItems();
+
+  console.log("PIC TO DOWNLOAD ARRAY");
+  console.log(picArray);
 };
 
 export const uploadPicsKCNA = async () => {
