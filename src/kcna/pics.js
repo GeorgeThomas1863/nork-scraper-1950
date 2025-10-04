439432c4cfbb26c80e220003d9aa1481b8a51f55
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-03 20:54:54 -0400

    dev

--- src/kcna/pics.js
+++ src/kcna/pics.js
@@ -1,3 +1,7 @@
+import path from "path";
+import fs from "fs";
+import axios from "axios";
+
 import CONFIG from "../../config/config.js";
 import dbModel from "../../models/db-model.js";
 
@@ -6,21 +10,22 @@ export const downloadPicsKCNA = async () => {
   const picModel = new dbModel({ keyExists: "url", keyEmpty: "picSize" }, pics);
   const picArray = await picModel.findEmptyItems();
 
-  console.log("PIC ARRAY");
-  console.log(picArray);
-  console.log(picArray.length);
-
   //CREATE PIC ID AND SAVE IT EARLIER IN PIC DB (maybe make last couple chars in URL?)
-  // for (const picItem of picArray) {
-  //   const { url } = picItem;
-  //   const picData = await downloadPicFS(url, savePath);
-
-  //   console.log("PIC TO DOWNLOAD ARRAY");
-  //   console.log(picArray);
-  // }
+  for (const picItem of picArray) {
+    try {
+      const { url, picId } = picItem;
+      const savePath = path.join(picPath, picId + ".jpg");
+      const picData = await downloadPicFS(url, savePath);
+    } catch (e) {
+      console.log(e.url + "; " + e.message + "; F BREAK: " + e.function);
+    }
+  }
 };
 
 export const downloadPicFS = async (url, savePath) => {
+  if (!url || !savePath) return null;
+  const { picProgressSize } = CONFIG;
+
   const picExists = fs.existsSync(savePath);
   if (picExists) {
     const error = new Error("PIC ALREADY DOWNLOADED");
@@ -28,6 +33,45 @@ export const downloadPicFS = async (url, savePath) => {
     error.function = "downloadPicFS";
     throw error;
   }
+
+  const res = await axios({
+    method: "get",
+    url: url,
+    timeout: 60 * 1000, //1 minute
+    responseType: "stream",
+  });
+
+  if (!res || !res.data) {
+    const error = new Error("AXIOS RES FUCKED");
+    error.url = url;
+    error.function = "downloadPicFS";
+    throw error;
+  }
+
+  console.log("!!!!!!");
+  console.log("DOWNLOAD PIC RES DATA");
+  console.log(res.data);
+
+  let downloadedSize = 0;
+
+  const writer = fs.createWriteStream(savePath);
+  const stream = res.data.pipe(writer);
+
+  res.data.on("data", (chunk) => {
+    // Log progress in KB every 100KB
+    downloadedSize += chunk.length;
+    if (downloadedSize % picProgressSize < chunk.length) {
+      const downloadedKB = Math.floor(downloadedSize / 1024);
+      console.log(`Downloaded: ${downloadedKB}KB`);
+    }
+  });
+  await new Promise((resolve, reject) => {
+    stream.on("finish", resolve);
+    stream.on("error", reject);
+  });
+
+  console.log(`DOWNLOAD COMPLETE: ${picId}.jpg | FINAL SIZE: ${Math.round(downloadedSize / 1024)}KB`);
+  return { downloadedSize: downloadedSize };
 };
 
 export const uploadPicsKCNA = async () => {
