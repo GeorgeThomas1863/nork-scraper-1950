273d10b82b59aae2d88c197a2b5c00201c925e0c
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-04 15:54:50 -0400

    still on video download dev

--- src/kcna/vids.js
+++ src/kcna/vids.js
@@ -6,7 +6,7 @@ import CONFIG from "../../config/config.js";
 import dbModel from "../../models/db-model.js";
 
 export const downloadVidsKCNA = async () => {
-  const { vids, vidPath, tempPath } = CONFIG;
+  const { vids, vidPath } = CONFIG;
   const vidModel = new dbModel({ keyExists: "url", keyEmpty: "vidSize" }, vids);
   const vidArray = await vidModel.findEmptyItems();
   if (!vidArray || !vidArray.length) return null;
@@ -17,64 +17,57 @@ export const downloadVidsKCNA = async () => {
   const downloadVidArray = [];
   for (const vidItem of vidArray) {
     try {
-      const { vidId, url } = vidItem;
+      const { vidId } = vidItem;
       vidItem.vidName = vidId + ".mp4";
       vidItem.savePath = path.join(vidPath, vidItem.vidName);
 
       const vidData = await downloadVidFS(vidItem);
+      downloadVidArray.push(vidData);
     } catch (e) {
       console.log(e.url + "; " + e.message + "; F BREAK: " + e.function);
     }
   }
+  return downloadVidArray;
 };
 
 export const downloadVidFS = async (inputParams) => {
   if (!inputParams) return null;
-  const { downloadVidChunkSize, tempPath, downloadVidConcurrent } = CONFIG;
-  const { url, savePath, vidName, vidId } = inputParams;
-
-  // console.log("INPUT PARAMS");
-  // console.log(inputParams);
+  const { downloadVidChunkSize, downloadVidConcurrent } = CONFIG;
+  const { url, vidId } = inputParams;
 
   const headers = await downloadVidHeaders(url);
   const vidSize = +headers["content-range"]?.substring(headers["content-range"]?.lastIndexOf("/") + 1, headers["content-range"]?.length); //in bytes
   const totalVidChunks = Math.ceil(vidSize / downloadVidChunkSize);
 
-  // console.log("VID SIZE");
-  // console.log(vidSize);
-  // console.log("TOTAL VID CHUNKS");
-  // console.log(totalVidChunks);
+  const downloadObj = { ...inputParams, headers: headers, vidSize: vidSize, totalVidChunks: totalVidChunks };
 
   //build chunk array so names / paths in one place
-  const chunkArray = await buildChunkArray(vidId, vidSize);
-  const downloadObj = { ...inputParams, headers: headers, vidSize: vidSize, totalVidChunks: totalVidChunks, chunkArray: chunkArray };
+  const chunkArrayDefault = await buildChunkArrayDefault(vidId, vidSize);
+  const chunkArrayCompleted = await getChunksCompleted(chunkArrayDefault);
+  const chunkArrayPending = chunkArrayDefault.filter((chunk) => !chunkArrayCompleted.includes(chunk));
 
-  console.log("CHUNK ARRAY");
-  console.log(chunkArray);
+  if (chunkArrayCompleted && chunkArrayCompleted.length === totalVidChunks) {
+    console.log("Vid already downloaded");
+    return null;
+  }
 
-  // const completedChunkArray = await getChunksCompleted(downloadObj);
-  // if (completedChunkArray && completedChunkArray.length === totalVidChunks) {
-  //   console.log("Vid already downloaded");
-  //   return null;
-  // }
+  if (chunkArrayCompleted && chunkArrayCompleted.length > 0) {
+    console.log(`Resuming Chunk ${chunkArrayCompleted.length + 1} of ${totalVidChunks} total chunks`);
+  }
 
-  // if (completedChunkArray && completedChunkArray.length > 0) {
-  //   console.log("Resuming Chunk " + completedChunkArray.length + " of " + totalVidChunks + " total chunks");
-  // }
+  for (let i = 0; i < chunkArrayPending.length; i += downloadVidConcurrent) {
+    const batchArray = chunkArrayPending.slice(i, i + downloadVidConcurrent);
+    const promiseArray = [];
 
-  // for (let i = 0; i < chunksToDownloadArray.length; i += downloadVidConcurrent) {
-  //   const batchArray = chunksToDownloadArray.slice(i, i + downloadVidConcurrent);
-  //   const promiseArray = [];
+    for (let j = 0; j < batchArray.length; j++) {
+      const chunkToDownload = batchArray[j];
+      const chunkObj = { ...chunkToDownload, ...downloadObj };
+      await downloadVidChunk(chunkObj);
+      promiseArray.push();
+    }
 
-  //   for (let j = 0; j < batchArray.length; j++) {
-  //     const chunkToDownload = batchArray[j];
-  //     const chunkObj = { ...chunkToDownload, ...downloadObj };
-  //     const chunkData = await downloadVidChunk(chunkObj);
-  //     promiseArray.push(chunkData);
-  //   }
-
-  //   const results = await Promise.allSettled(promiseArray);
-  // }
+    const results = await Promise.allSettled(promiseArray);
+  }
 };
 
 //res.headers doesnt work, so getting headers by getting small number of bytes
@@ -98,11 +91,14 @@ export const downloadVidHeaders = async (url) => {
     throw error;
   }
 
+  console.log("RES HEADERS");
+  console.log(resHeaders);
+
   const headers = { ...resHeaders.headers };
   return headers;
 };
 
-export const buildChunkArray = async (vidId, vidSize) => {
+export const buildChunkArrayDefault = async (vidId, vidSize) => {
   if (!vidId || !vidSize) return null;
   const { tempPath, downloadVidChunkSize } = CONFIG;
 
@@ -124,22 +120,21 @@ export const buildChunkArray = async (vidId, vidSize) => {
       chunkSize: chunkSize,
     };
 
-    console.log("CHUNK OBJ");
-    console.log(chunkObj);
+    // console.log("CHUNK OBJ");
+    // console.log(chunkObj);
 
     chunkArray.push(chunkObj);
   }
   return chunkArray;
 };
 
-export const getChunksCompleted = async (inputObj) => {
-  if (!inputObj) return null;
-  const { chunkArray } = inputObj;
+export const getChunksCompleted = async (inputArray) => {
+  if (!inputObj || !inputObj.length) return null;
 
   //loop through and see if any chunks already downloaded
   const completedChunkArray = [];
-  for (let i = 0; i < chunkArray.length; i++) {
-    const chunk = chunkArray[i];
+  for (let i = 0; i < inputArray.length; i++) {
+    const chunk = inputArray[i];
     const { chunkPath, chunkSize } = chunk;
 
     const chunkStored = fs.existsSync(chunkPath);
@@ -158,52 +153,47 @@ export const getChunksCompleted = async (inputObj) => {
   return completedChunkArray;
 };
 
-// export const getChunksPending = async (inputObj) => {
-//   if (!inputObj) return null;
-//   const { downloadVidChunkSize, tempPath } = CONFIG;
-//   const { totalVidChunks, vidName, vidSize, chunkArray } = inputObj;
-
-//   const chunksToDownloadArray = [];
-//   for (let i = 0; i < chunkArray.length; i++) {
-//     const chunk = chunkArray[i];
-//     const { chunkPath } = chunk;
-
-//     //skip chunks already downloaded
-//     const chunkExists = fs.existsSync(chunkPath);
-//     if (chunkExists) continue;
-
-//     const startByte = i * downloadVidChunkSize;
-//     const endByte = Math.min(startByte + downloadVidChunkSize - 1, vidSize - 1);
-//     if (endByte < startByte || !startByte || !endByte) continue;
-
-//     const downloadParams = {
-//       index: i,
-//       chunkName: chunkName,
-//       chunkPath: chunkPath,
-//       startByte: startByte,
-//       endByte: endByte,
-//     };
-
-//     chunksToDownloadArray.push(downloadParams);
-//   }
-
-//   return chunksToDownloadArray;
-// };
-
 export const downloadVidChunk = async (inputObj) => {
   if (!inputObj) return null;
-  const { url, savePath, vidName, startByte, endByte } = inputObj;
+  const { url, chunkPath, startByte, endByte } = inputObj;
 
-  const res = await axios({
-    method: "get",
-    url: url,
-    timeout: 60 * 1000, //1 minute
-    responseType: "stream",
-    headers: {
-      Range: `bytes=${startByte}-${endByte}`,
-    },
-  });
+  try {
+    console.log("DOWNLOADING CHUNK");
+    console.log(inputObj);
+
+    const res = await axios({
+      method: "get",
+      url: url,
+      timeout: 60 * 1000, //1 minute
+      responseType: "stream",
+      headers: {
+        Range: `bytes=${startByte}-${endByte}`,
+      },
+    });
+
+    if (!res || !res.data) {
+      const error = new Error("CHUNK DOWNLOAD FUCKED");
+      error.url = url;
+      error.function = "downloadVidChunk";
+      throw error;
+    }
+
+    const writer = fs.createWriteStream(chunkPath);
+    res.data.pipe(writer);
+
+    await new Promise((resolve, reject) => {
+      writer.on("finish", resolve);
+      writer.on("error", reject);
+      res.data.on("error", reject);
+    });
+
+    return true;
+  } catch (e) {
+    console.log(e.url + "; " + e.message + "; F BREAK: " + e.function);
+    return null;
+  }
 };
+
 //--------------------------
 
 export const uploadVidsKCNA = async () => {
