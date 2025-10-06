126475157846568f8184adae45545a9ba917d6e3
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-06 11:22:59 -0400

    dev

--- src/kcna/articles.js
+++ src/kcna/articles.js
@@ -279,31 +279,56 @@ export const uploadArticlesKCNA = async () => {
   const articleArray = await articleModel.findEmptyItems();
   if (!articleArray || !articleArray.length) return null;
 
+  const articlePostDataArray = [];
   for (const article of articleArray) {
-    const { url, date, picArray } = article;
+    try {
+      const { url } = article;
 
-    //normalize url and date
-    const tgInputs = await normalizeTGInputs(url, date);
-    const uploadObj = { ...article, ...tgInputs };
+      //add channelId HERE
+      article.tgChannelId = tgChannelId;
 
-    //channel to upload to
-    uploadObj.tgChannelId = tgChannelId;
+      //post article
+      const articlePostData = await postArticleTG(article);
+      if (!articlePostData) continue;
+      articlePostDataArray.push(articlePostData);
 
-    const titleData = await postArticleTitleTG(uploadObj);
-    console.log("TITLE DATA");
-    console.log(titleData);
+      console.log("ARTICLE POST DATA");
+      console.log(articlePostData);
 
-    //post pics if exist
-    if (picArray && picArray.length) {
-      const articlePicsData = await postArticlePicsTG(uploadObj);
-      console.log("ARTICLE PICS DATA");
-      console.log(articlePicsData);
+      //store data
+      const storeModel = new dbModel({ keyToLookup: "url", itemValue: url, updateObj: articlePostData }, articles);
+      const storeData = await storeModel.updateObjItem();
+      console.log("STORE DATA");
+      console.log(storeData);
+    } catch (e) {
+      console.log(e.url + "; " + e.message + "; F BREAK: " + e.function);
     }
-
-    const articleContentData = await postArticleContentTG(uploadObj);
-    console.log("ARTICLE CONTENT DATA");
-    console.log(articleContentData);
   }
+
+  return articlePostDataArray;
+};
+
+export const postArticleTG = async (inputObj) => {
+  if (!inputObj) return null;
+  const { url, date, picArray } = inputObj;
+
+  //normalize url and date
+  const tgInputs = await normalizeTGInputs(url, date);
+  const uploadObj = { ...inputObj, ...tgInputs };
+
+  const titleData = await postArticleTitleTG(uploadObj);
+  uploadObj.titleData = titleData;
+
+  //post pics if exist
+  if (picArray && picArray.length) {
+    const articlePicsData = await postArticlePicsTG(uploadObj);
+    uploadObj.articlePicsData = articlePicsData;
+  }
+
+  const articleContentData = await postArticleContentTG(uploadObj);
+  uploadObj.articleContentData = articleContentData;
+
+  return uploadObj;
 };
 
 export const postArticleTitleTG = async (inputObj) => {
@@ -412,7 +437,7 @@ export const buildShortText = async (inputObj) => {
 
 ${text}
 
-<i>${urlNormal}</i>
+<b>URL:</b> <i>${urlNormal}</i>
 `;
 
   return shortText;
@@ -455,7 +480,7 @@ export const buildChunkText = async (chunk, inputObj, chunkIndex) => {
       return "<b>[ARTICLE TEXT]:</b>" + "\n\n" + chunk;
 
     case chunkTotal - 1:
-      return chunk + "\n\n" + "<i>" + urlNormal + "</i>";
+      return chunk + "\n\n" + "<b>URL:</b> <i>" + urlNormal + "</i>";
 
     default:
       return chunk;
