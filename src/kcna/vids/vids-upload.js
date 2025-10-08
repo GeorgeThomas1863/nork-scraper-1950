e573c413574ef2d65c4ff83f1d9f137b0bd60f83
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-08 19:46:59 -0400

    dev

--- src/kcna/vids/vids-upload.js
+++ src/kcna/vids/vids-upload.js
@@ -25,15 +25,54 @@ export const buildVidThumbnailCaption = async (inputObj) => {
 
   const captionText = `🇰🇵 🇰🇵 🇰🇵
   
-  -----------------
+-----------------
         
-  <b>VID TITLED: ${title}</b>
+<b>VID TITLED: ${title}</b>
       
-  -----------------
+-----------------
   
-<b>${chunkCount} CHUNK VID</b> | <b>VID PAGE ID:</b> ${vidPageId} | <b>DATE:</b> <i>${dateNormal}</i> | <b>VID PAGE URL:</b> 
+<b>${chunkCount} CHUNK VID</b> | <b>VID PAGE ID:</b> ${vidPageId} | <b>DATE:</b> <i>${dateNormal}</i> | <b>URL:</b> 
 <i>${url}</i>
       `;
 
   return captionText;
+};
+
+//----------------------------
+
+export const postVidTG = async (inputObj) => {
+  if (!inputObj) return null;
+
+  const vidForm = await buildVidForm(inputObj);
+};
+
+export const buildVidForm = async (inputObj) => {
+  if (!inputObj) return null;
+  //   const { uploadPath, tgUploadId, uploadFileName } = inputObj;
+
+  console.log("BUILD VID FORM INPUT OBJ");
+  console.log(inputObj);
+
+  //   const readStream = fs.createReadStream(uploadPath);
+
+  //   // Create form data for this chunk
+  //   const formData = new FormData();
+  //   formData.append("chat_id", tgUploadId);
+  //   formData.append("video", readStream, {
+  //     filename: uploadFileName,
+  //   });
+
+  //   //set setting for auto play / streaming
+  //   formData.append("supports_streaming", "true");
+  //   formData.append("width", "1280");
+  //   formData.append("height", "720");
+
+  //   if (!formData || !readStream) {
+  //     const error = new Error("BUILD VID FORM FUCKED");
+  //     error.content = "FORM DATA: " + formData;
+  //     error.function = "buildVidForm";
+  //     throw error;
+  //   }
+
+  //   return formData;
 };
