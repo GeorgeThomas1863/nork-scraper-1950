3c3114366cff66207dbfd9b9afec94bff0d8e127
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-10-09 20:00:14 -0400

    dev

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -15,31 +15,19 @@ export const apiIncomingController = async (req, res) => {
   return res.json(data);
 };
 
-// export const apiOutgoingController = async (req, res) => {
-//   try {
-//     const inputParams = req.body;
-//     console.log("API OUTGOING DATA");
-//     console.log(inputParams);
-
-//     const data = await handleOutgoingAPI(inputParams);
-//     console.log("API OUTGOING RESPONSE");
-//     console.log(data);
-
-//     return res.json(data);
-//   } catch (e) {
-//     console.error(e);
-//     return res.status(500).json({ e: "Failed to get admin backend data" });
-//   }
-// };
-
 export const apiOutgoingController = async (req, res) => {
-  const inputParams = req.body;
-  console.log("API OUTGOING DATA");
-  console.log(inputParams);
+  try {
+    const inputParams = req.body;
+    console.log("API OUTGOING DATA");
+    console.log(inputParams);
 
-  const data = await handleOutgoingAPI(inputParams);
-  console.log("API OUTGOING RESPONSE");
-  console.log(data);
+    const data = await handleOutgoingAPI(inputParams);
+    console.log("API OUTGOING RESPONSE");
+    console.log(data);
 
-  return res.json(data);
+    return res.json(data);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ e: "Failed to get admin backend data" });
+  }
 };
